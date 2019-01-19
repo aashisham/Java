3cf4f1e841821851a62fa288df5188c9074550a2
Ashish Poudel <33601771+ampmboy@users.noreply.github.com>
2019-01-19 20:09:29 +0545

    Create MyException.java
    creating my exception class

---
+++ Exception/Own Exception/MyException.java
@@ -0,0 +1,13 @@
+class MyException extends Exception
+{
+	private int detail;
+	MyException(int a)
+	{
+		detail = a;
+	}
+	public String toString()
+	{
+		return "MyException["+detail+"]";
+	}
+
+}
