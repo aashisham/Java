8de52b6cfd10242688cf33b4f99f630cbb93807c
Ashish Poudel <33601771+ampmboy@users.noreply.github.com>
2019-01-23 22:28:46 +0545

    access modifier

---
+++ OOP/AccessModifier/MovieDemo.java
@@ -0,0 +1,11 @@
+class MovieDemo{
+
+	public static void main(String[] args){
+
+		Movie movie = new Movie();
+		movie.name = "munna vai MBBS";
+		movie.producer = "ashish";
+		// movie.setName = "munna vai MBBS";
+		
+	}
+}
