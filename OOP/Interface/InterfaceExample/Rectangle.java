084b1e124d46a077a420e989f6043b0ef138cc32
Ashish Poudel <33601771+ampmboy@users.noreply.github.com>
2019-01-23 21:27:15 +0545

    implementation of interface

---
+++ OOP/Interface/InterfaceExample/Rectangle.java
@@ -0,0 +1,12 @@
+class Rectangle implements Shape,Graphics
+{
+	public void area()
+	{
+		System.out.println("area in rectangle");
+
+	}
+	public void draw()
+	{
+		System.out.println("graphics in rectangle");
+	}
+}
