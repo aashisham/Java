e3611df142692e9aa1672a8f254747a0a457d3ce
Ashish Poudel <33601771+ampmboy@users.noreply.github.com>
2019-01-23 22:14:23 +0545

    implements abstract class

---
+++ OOP/AbstractClass/AbstractClassTwo/Figure.java
@@ -0,0 +1,12 @@
+abstract class Figure //this is abstract class
+{
+	
+	double dim1 , dim2 ;
+	Figure (double a , double b)
+	{
+		dim1 = a ;
+		dim2 = b ;
+	}
+	abstract double area(); //this is abstract method
+}
+
