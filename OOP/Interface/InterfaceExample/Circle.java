fa439642f7a149fcd1c1965935fdbe416fb01d89
Ashish Poudel <33601771+ampmboy@users.noreply.github.com>
2019-01-23 21:23:35 +0545

    interface

---
+++ OOP/Interface/InterfaceExample/Circle.java
@@ -0,0 +1,17 @@
+class Circle implements Shape,Graphics
+{
+	public void area()
+	{
+		System.out.println("area in circle");
+
+	}
+	public void draw()
+	{
+		System.out.println("graphics in circle");
+	}
+
+	// void init()
+	// {
+	// 	Shape.super.init();
+	// }
+}
