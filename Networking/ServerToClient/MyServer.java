bbea1f8cb35c65fbf716d640c0aef5c46ef0f2c9
Ashish Poudel <33601771+ampmboy@users.noreply.github.com>
2019-02-16 23:38:57 +0545

    server section of server-client messaging

---
+++ Networking/ServerToClient/MyServer.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+
+public class MyServer {
+	public static void main(String[] args)
+	{
+		try {
+			
+			//step 1 : first ,server should instatiate ServerSocket object
+			ServerSocket ss = new ServerSocket(6666);
+
+			// Step 2 : Then, Server object envokes accept() method of ServerSocket class. This waits for client until client creates Socket object.
+			Socket s = ss.accept();  //establish connection 
+			
+			DataInputStream dis =new DataInputStream(s.getInputStream());
+
+			String str = (String)dis.readUTF();
+
+			System.out.print("message"+str);
+
+			ss.close();
+		}
+		catch(Exception e) {
+			System.out.print(e);
+		}
+		
+	}
+
+}
