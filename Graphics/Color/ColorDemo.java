9fb81f5954d59f6ccebb7836601198dbaaf85657
Ashish Poudel <33601771+ampmboy@users.noreply.github.com>
2019-02-16 21:21:31 +0545

    filling and setting color using applet

---
+++ Graphics/Color/ColorDemo.java
@@ -0,0 +1,17 @@
+//print hello world in red color and gces in green and create red and green rectangle using applet
+import java.awt.*;
+import java.applet.Applet;
+
+/* <applet code = "ColorDemo" height = "300" width = "300"></applet> */
+
+public class ColorDemo extends Applet {
+    public void paint(Graphics g) {
+        g.setColor(new Color(255, 0, 0));
+        g.fillRect(60, 60, 40, 40);
+        g.drawString("Hello World", 40, 40);
+        g.setColor(new Color(0, 255, 0));
+        g.fillRect(90, 60, 40, 40);
+        g.drawString("GCES", 40, 80);
+
+    }
+}
